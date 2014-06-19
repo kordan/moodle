3d7c0d70a3bff2c3b8357801ed22610618155857
Kordan <kordan@mclink.it>
2014-06-19 13:41:27 +0200

    NOBUG theme_frame: cut abnormal margin-top for pages using maintenance layout

--- theme/frame/layout/maintenance.php
+++ theme/frame/layout/maintenance.php
@@ -34,7 +34,7 @@ echo $OUTPUT->doctype() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<body <?php echo $OUTPUT->body_attributes(); ?>>
+<body <?php echo $OUTPUT->body_attributes(array('nonavbar')); ?>>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
